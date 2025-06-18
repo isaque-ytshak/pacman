f9472dd81881123af9cdffb4430ba6f98c1235bb
isaque-ytshak <isaque.rodrigues16@escola.pr.gov.br>
2025-06-18 14:28:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const pacman = document.getElementById("pacman");
+
+let x = 0;
+let y = 0;
+
+const speed = 10;
+const gameSize = 460;
+
+document.addEventListener("keydown", (e) => { 
+switch (e.key) {
+    case "ArrowUp":
+        if (y > 0) y -= speed;
+        pacman.style.transform = "rotate(-90deg)";
+    break;
+    case "ArrowDown":
+        if (y > gameSize) y += speed;
+     pacman.style.transform = "rotate(-90deg)";
+    break;
+    case "ArrowLeft":
+        if (x > 0) x -= speed;
+    pacman.style.transform = "rotatate(0deg)"
+    break;
+}
+pacman.style.left = x + "px";
+pacman.style.top =y + "px"
+})
